ab09a89f52b3c4206288c9959d8838791978dff5
Adrian <danielgarcia84.pro@gmail.com>
2026-04-14 13:03:03 -0300

    feat: add health and func to app and server, and check health

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-import env from './config/env'
+import env from './config/env.js'
 import authRoutes from './modules/auth/auth.routes.js';
 import notFoundMiddleware from './middlewares/notFound.middleware.js';
 import errorMiddleware from './middlewares/error.middleware.js';
@@ -18,13 +18,18 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('api/health', (req, res) => {
+app.use('/api/auth', authRoutes);
+
+app.get('/api/health', (req, res) => {
     res.status(200).json({
         message: "Backend is running",
     });
 });
 
-app.use('api/auth', authRoutes);
+app.get('/hello-check-123', (req, res) => {
+  res.json({ message: 'THIS IS MY REAL APP' });
+});
+
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
