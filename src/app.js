3e5ff357915ff5ba2096334ce85fb5ed00279b5e
Adrian <danielgarcia84.pro@gmail.com>
2026-04-14 14:26:36 -0300

    feat: add database health check route

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import env from './config/env.js'
 import authRoutes from './modules/auth/auth.routes.js';
+import healthRoutes from './modules/health/health.routes.js';
 import notFoundMiddleware from './middlewares/notFound.middleware.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/health', healthRoutes);
 
 app.get('/api/health', (req, res) => {
     res.status(200).json({
