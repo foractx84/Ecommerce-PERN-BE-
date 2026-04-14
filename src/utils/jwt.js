e7d9c2b4e567aee5fd76c864139c711071ae0476
Adrian <danielgarcia84.pro@gmail.com>
2026-04-14 18:17:14 -0300

    feat: add jwt utility for access tokens

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,1 +1,16 @@
-export {};
+import jwt from 'jsonwebtoken';
+import env from '../config/env.js';
+
+export function generateAccessToken(user) {
+  return jwt.sign(
+    {
+      sub: user.id,
+      email: user.email,
+      role: user.role,
+    },
+    env.jwtAccessSecret,
+    {
+      expiresIn: env.jwtAccessExpiresIn,
+    }
+  );
+}
