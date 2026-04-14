a2c46ee0755c9b77c4f0dbc3f0f673ce5a653489
Adrian <danielgarcia84.pro@gmail.com>
2026-04-14 12:16:57 -0300

    feat: bootstrap express app and auth module routing

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import env from './config/env'
+import authRoutes from './modules/auth/auth.routes.js';
+
+const app = express();
+
+app.use(
+    cors({
+        origin: env.clientUrl,
+        credentials: true,
+    })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('api/health', (req, res) => {
+    res.status(200).json({
+        message: "Backend is running",
+    });
+});
+
+app.use('api/auth', authRoutes);
+
+export default app;
