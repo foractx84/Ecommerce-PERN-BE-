0e130d8a028b60395d428c0ae3cc0a7e3dfcc26a
Adrian <danielgarcia84.pro@gmail.com>
2026-04-14 16:58:25 -0300

    fix: git issues

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,11 +1,14 @@
-import { Router } from "express";
+import { Router } from 'express';
+import { register } from './auth.controller.js';
 
 const router = Router();
 
+router.post('/register', register);
+
 router.get('/test', (req, res) => {
-    res.status(200).json({
-        message: 'Auth route is working',
-    });
+  res.status(200).json({
+    message: 'Auth route is working',
+  });
 });
 
 export default router;
